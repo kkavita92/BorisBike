7aefa9a4c7bba4ee9e41d03d9329cb162344bfe8
charliefea <charlotte.feather@yahoo.com>
2017-05-09 15:25:18 +0100

    Compeleted Challenge 10

--- spec/dockingstation_spec.rb
+++ spec/dockingstation_spec.rb
@@ -1,4 +1,14 @@
 require 'dockingstation'
+require 'bike'
 describe DockingStation do
   it { is_expected.to respond_to(:release_bike) }
+
+  it 'is of class of station' do
+    expect(subject).to be_instance_of DockingStation
+  end
+
+  it 'releases a bike' do
+    bike = subject.release_bike
+    expect(bike).to be_working
+  end
 end
