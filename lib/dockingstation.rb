b35de0bfd51c7cb0fa78f315833261e6e5e87e83
Kavita <kkavita92@gmail.com>
2017-05-10 23:25:37 +0100

    complete challenge 15

--- lib/dockingstation.rb
+++ lib/dockingstation.rb
@@ -6,13 +6,23 @@ class DockingStation
     @bikes = []
   end
   def release_bike
-    fail("No bikes left") if @bikes.empty? #guard condition
+    fail("No bikes left") if empty? #guard condition
     @bikes.pop
   end
 
   def dock(bike) #docks a bike instance
-    fail("No spaces left") if @bikes.count >= 20 #error if bike already has been docked
+    fail("No spaces left") if full? #error if bike already has been docked
     @bikes.push(bike) #saves docked bike into memory so we can see it later
+  end
+
+  private
+
+  def full?
+    @bikes.count >= 20
+  end
+
+  def empty?
+    @bikes.empty?
   end
 
 end
--- spec/dockingstation_spec.rb
+++ spec/dockingstation_spec.rb
@@ -2,10 +2,11 @@ require 'dockingstation'
 
 describe DockingStation do
   it { is_expected.to respond_to(:release_bike) }
+  it { is_expected.to respond_to(:dock).with(1).argument }
 
+#TESTS FOR RELEASE_BIKE
   describe '#release_bike' do
-
-    it 'no release if no bike' do
+    it 'raises error when releasing bike from empty docking station' do
       expect {subject.release_bike}.to raise_error{"No bikes left"}
     end
 
@@ -14,11 +15,10 @@ describe DockingStation do
       subject.dock(bike)
       expect(bike).to be_working
     end
-
   end
 
+#TESTS FOR DOCK
   describe '#dock' do
-
     it 'docks a bike' do
       bike = Bike.new
       expect(subject.dock(bike)).to eq [bike]
@@ -28,11 +28,9 @@ describe DockingStation do
       20.times { subject.dock Bike.new }
       expect {subject.dock(Bike.new)}.to raise_error{"No spaces left"}
     end
-
-
   end
 
-  it 'returns docked bikes' do
+  it 'returns docked bikes' do #CHECK IF CORRECT!!!!
     bike = Bike.new
     subject.dock(bike) #needs to remember which bike is docked
     expect(subject.bikes[-1]).to eq bike #returns bike
